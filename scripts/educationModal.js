de37db604e2c23a0d07f84ed78d4d47096808d23
supercyanide <61550807+supercyanide@users.noreply.github.com>
2023-04-10 03:27:19 +0300

    lol

---
+++ scripts/educationModal.js
@@ -0,0 +1,19 @@
+
+// const contactInfoPopup = document.getElementById('modalId')
+// contactInfoPopup.addEventListener('show.bs.modal', event => {
+//   const person = event.relatedTarget;
+//   const recipient = person.getAttribute('data-bs-name');
+//   const room = person.getAttribute('data-bs-room');
+//   const email = person.getAttribute('data-bs-email');
+//   const phone = person.getAttribute('data-bs-phone');
+//   const modalTitle = contactInfoPopup.querySelector('#contactInfoPopupLabel');
+//   const modalRoom = contactInfoPopup.querySelector('#contactInfoPopupRoom');
+//   const modalEmail = contactInfoPopup.querySelector('#contactInfoPopupEmail');
+//   const modalPhone = contactInfoPopup.querySelector('#contactInfoPopupPhone');
+
+
+//   modalTitle.textContent = recipient;
+//   modalRoom.textContent = room;
+//   modalEmail.textContent = email;
+//   modalPhone.textContent = phone;
+// })
--- scripts/scriptFaculty.js
+++ scripts/scriptFaculty.js
@@ -1,3 +1,70 @@
+$(document).ready(function(){
+    $('.crsl').slick({
+      arrows: true,
+      centerMode: true,
+      centerPadding: '0',
+      slidesToShow: 1,
+      variableWidth: true,
+      prevArrow: document.querySelector('.arw-left'),
+      nextArrow: document.querySelector('.arw-right'),
+      responsive: [
+        {
+            breakpoint: 1600,
+            settings: {
+                centerMode: true,
+                slidesToShow: 1,
+                
+            }
+        },
+        {
+        breakpoint: 1280,
+        settings: {
+            centerMode: true,
+            centerPadding: '100px 0',
+            slidesToShow: 1,
+            
+        }
+        },
+        {
+        breakpoint: 1024,
+        settings: {
+            centerMode: true,
+            // centerPadding: '40px',
+            slidesToShow: 1,
+            
+        }
+        },
+        {
+        breakpoint: 900,
+        settings: {
+            centerMode: true,
+            // centerPadding: '40px',
+            slidesToShow: 1,
+            
+        }
+        },
+        {
+        breakpoint: 768,
+        settings: {
+            centerMode: true,
+            // centerPadding: '40px',
+            slidesToShow: 1,
+            
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            centerMode: true,
+            // centerPadding: '40px',
+            slidesToShow: 1,
+        }
+        },
+        
+          
+      ]
+    });
+  });
 function burgerMenu(selector) {
     let menu = $(selector);
     let button = menu.find('.burger-menu_button', '.burger-menu_lines');
@@ -26,35 +93,35 @@ function burgerMenu(selector) {
 burgerMenu('.burger-menu');
 
 
-function slider() {
-    const images = document.querySelectorAll('.slider_line .card');
-    const sliderLine = document.querySelector('.slider_line');
-    let activeSlide = 0;
-    let width;
+// function slider() {
+//     const images = document.querySelectorAll('.slider_line .card');
+//     const sliderLine = document.querySelector('.slider_line');
+//     let activeSlide = 0;
+//     let width;
 
-    function init() {
-        width = document.querySelector('.slider').offsetWidth;
-        sliderLine.style.width = width * images.length + 'px';
-        images.forEach(item => {
-            item.style.width = width + 'px';
-            item.style.height = 'auto';
-        })
-        rollSlider();
-    }
-    init()
-    document.querySelector('.carousel_right_btn').addEventListener('click', function () {
-        activeSlide++;
-        if (activeSlide >= images.length) activeSlide = 0;
-        rollSlider();
-    })
-    document.querySelector('.carousel_left_btn').addEventListener('click', function () {
-        activeSlide--;
-        if (activeSlide < 0) activeSlide = images.length - 1;
-        rollSlider();
-    })
-    function rollSlider() {
-        sliderLine.style.transform = 'translate(-' + activeSlide * width + 'px)';
-    }
-}
-window.addEventListener('resize', slider);
-slider();
+//     function init() {
+//         width = document.querySelector('.slider').offsetWidth;
+//         sliderLine.style.width = width * images.length + 'px';
+//         images.forEach(item => {
+//             item.style.width = width + 'px';
+//             item.style.height = 'auto';
+//         })
+//         rollSlider();
+//     }
+//     init()
+//     document.querySelector('.carousel_right_btn').addEventListener('click', function () {
+//         activeSlide++;
+//         if (activeSlide >= images.length) activeSlide = 0;
+//         rollSlider();
+//     })
+//     document.querySelector('.carousel_left_btn').addEventListener('click', function () {
+//         activeSlide--;
+//         if (activeSlide < 0) activeSlide = images.length - 1;
+//         rollSlider();
+//     })
+//     function rollSlider() {
+//         sliderLine.style.transform = 'translate(-' + activeSlide * width + 'px)';
+//     }
+// }
+// window.addEventListener('resize', slider);
+// slider();
